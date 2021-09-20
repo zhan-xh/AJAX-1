4cbbccdf007a5377b5490e8e948e1505b7342d91
xiaohongZHAN <2459522339@qq.com>
2021-09-20 12:30:00 +0800

    加载xml，json，分页

--- public/main.js
+++ public/main.js
@@ -10,6 +10,50 @@ getJS.onclick = () => {
     request.onerror = () => {}
     request.send()
 };
+let n = 1
+getPage.onclick = () => {
+    const request = new XMLHttpRequest()
+    request.open("GET", `page${n+1}`)
+    request.onreadystatechange = () => {
+        if (request.readyState === 4 && request.status === 200) {
+            const array = JSON.parse(request.response)
+            array.forEach(item => {
+                const li = document.createElement('li')
+                li.textContent = item.id
+                xxx.appendChild(li)
+            });
+            n += 1
+        }
+    };
+    request.send()
+};
+getJSON.onclick = () => {
+    const request = new XMLHttpRequest()
+    request.open("GET", "/5.json")
+    request.onreadystatechange = () => {
+        if (request.readyState === 4 && request.status === 200) {
+            console.log(request.response)
+            const object = JSON.parse(request.response)
+            myName.textContent = object.name
+            console.log(object)
+        }
+    }
+    request.send()
+
+};
+getXML.onclick = () => {
+    const request = new XMLHttpRequest();
+    request.open('GET', '/4.xml')
+    request.onreadystatechange = () => {
+        if (request.readyState === 4 && request.status === 200) {
+            // console.log(request.response)
+            const dom = request.responseXML
+            const text = dom.getElementsByTagName('warning')[0].textContent
+            console.log(text)
+        }
+    };
+    request.send()
+};
 getHTML.onclick = () => {
     const request = new XMLHttpRequest();
     request.open('GET', '/3.html');
--- server.js
+++ server.js
@@ -26,7 +26,11 @@ var server = http.createServer(function (request, response) {
     if (path === '/index.html') { //路由1
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        const string = fs.readFileSync('public/index.html')
+        let string = fs.readFileSync('public/index.html').toString()
+        const page1 = fs.readFileSync('db/page1.json').toString()
+        const array = JSON.parse(page1)
+        const result = array.map(item => `<li>${item.id}</li>`).join('')
+        string = string.replace('{{page1}}', `<ul id="xxx">${result}</ul>`)
         response.write(string)
         response.end()
     } else if (path === '/main.js') { //路由2
@@ -49,6 +53,26 @@ var server = http.createServer(function (request, response) {
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
         response.write(fs.readFileSync("public/3.html"))
+        response.end()
+    } else if (path === '/4.xml') {
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/xml;charset=utf-8')
+        response.write(fs.readFileSync("public/4.xml"))
+        response.end()
+    } else if (path === '/5.json') {
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/json;charset=utf-8')
+        response.write(fs.readFileSync("public/5.json"))
+        response.end()
+    } else if (path === '/page2') {
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/json;charset=utf-8')
+        response.write(fs.readFileSync("db/page2.json"))
+        response.end()
+    } else if (path === '/page3') {
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/json;charset=utf-8')
+        response.write(fs.readFileSync("db/page3.json"))
         response.end()
     } else {
         response.statusCode = 404
